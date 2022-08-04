f7ee6362d4ef4f2acd7a9ae163a204895a33e292
mcantillon21 <60069300+mcantillon21@users.noreply.github.com>
2022-08-04 13:54:08 -0400

    initial commit
    Co-authored-by: Fabio Ibanez <fabioibanez@users.noreply.github.com>

--- client/react-cra/src/components/Checkout.js
+++ client/react-cra/src/components/Checkout.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const formatPrice = ({ amount, currency, quantity }) => {
-  const numberFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
+  const numberFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
     currency,
-    currencyDisplay: 'symbol',
+    currencyDisplay: "symbol",
   });
   const parts = numberFormat.formatToParts(amount);
   let zeroDecimalCurrency = true;
   for (let part of parts) {
-    if (part.type === 'decimal') {
+    if (part.type === "decimal") {
       zeroDecimalCurrency = false;
     }
   }
@@ -19,17 +19,50 @@ const formatPrice = ({ amount, currency, quantity }) => {
 };
 
 const Checkout = () => {
-  const [quantity, setQuantity] = useState(1);
   const [amount, setAmount] = useState(0);
-  const [currency, setCurrency] = useState('USD');
+  const [currency, setCurrency] = useState("USD");
+  const [val, setVal] = useState(undefined);
+  const [balance, setBalance] = useState(0);
+  const [button1, setButton1] = useState('Check Balance');
+  const [button2, setButton2] = useState('Check Balance');
+  const [button3, setButton3] = useState('Check Balance');
+
+  function onChange(e) {
+    const re = /^[0-9\b]+$/;
+    if (e.target.value === "" || re.test(e.target.value)) {
+      setVal(e.target.value);
+    }
+  }
+
+  async function addBalance1() {
+    let RandomNumber = Math.floor(Math.random() * 100) + 1;
+    const curBalance = balance + RandomNumber
+    setBalance(curBalance);
+    setButton1('Redeemed');
+  }
+
+  async function addBalance2() {
+    let RandomNumber = Math.floor(Math.random() * 100) + 1;
+    const curBalance = balance + RandomNumber
+    setBalance(curBalance);
+    setButton2('Redeemed');
+  }
+
+  async function addBalance3() {
+    let RandomNumber = Math.floor(Math.random() * 100) + 1;
+    const curBalance = balance + RandomNumber
+    setBalance(curBalance);
+    setButton3('Redeemed');
+  }
+
+  // button.addEventListener('click', setButtonColor('green'));
 
   useEffect(() => {
     async function fetchConfig() {
       // Fetch config from our backend.
-      const {
-        unitAmount,
-        currency
-      } = await fetch('/config').then(r => r.json());
+      const { unitAmount, currency } = await fetch("/config").then((r) =>
+        r.json()
+      );
       setAmount(unitAmount);
       setCurrency(currency);
     }
@@ -38,54 +71,74 @@ const Checkout = () => {
 
   return (
     <div className="sr-root">
+      <div className="sr-header">
+        <button2>Current Balance: {balance}</button2>
+        <h1 style={{float: 'left'}}>Get paid now!</h1>
+      </div>
       <div className="sr-main">
         <section className="container">
           <div>
-            <h1>Single photo</h1>
-            <h4>Purchase a Pasha original photo</h4>
+            <h1>Apple</h1>
             <div className="pasha-image">
               <img
                 alt="Random asset from Picsum"
-                src="https://picsum.photos/280/320?random=4"
-                width="140"
+                src="https://media.idownloadblog.com/wp-content/uploads/2018/07/Apple-logo-black-and-white-768x895.png"
+                width="160"
                 height="160"
               />
             </div>
           </div>
-          <form action="/create-checkout-session" method="POST">
+          <div>
+            <p className="sr-legal-text">Enter your Apple Gift Card Number</p>
             <div className="quantity-setter">
-              <button
-                className="increment-btn"
-                disabled={quantity === 1}
-                onClick={() => setQuantity(quantity - 1)}
-                type="button"
-              >
-                -
-              </button>
-              <input
-                type="number"
-                id="quantity-input"
-                min="1"
-                max="10"
-                value={quantity}
-                name="quantity"
-                readOnly
-              />
-              <button
-                className="increment-btn"
-                disabled={quantity === 10}
-                onClick={() => setQuantity(quantity + 1)}
-                type="button"
-              >
-                +
-              </button>
+              <input type="number" pattern="^-?[0-9]\d*\.?\d*$" />
             </div>
-            <p className="sr-legal-text">Number of copies (max 10)</p>
-
-            <button role="link" id="submit" type="submit">
-              Buy {formatPrice({amount, currency, quantity})}
-            </button>
-          </form>
+            <button onClick={()=> addBalance1()} style={{background:button1=='Redeemed'?"#32cd32":""}}>{button1}</button>
+          </div>
+        </section>
+        <section className="container">
+          <div>
+            <h1>Allbirds</h1>
+            <div className="pasha-image">
+              <img
+                alt="Random asset from Picsum"
+                src="https://thegreenhubonline.com/wp-content/uploads/2019/03/Allbirds-logo.jpg"
+                width="160"
+                height="160"
+              />
+            </div>
+          </div>
+          <div>
+            <p className="sr-legal-text">
+              Enter your Allbirds Gift Card Number
+            </p>
+            <div className="quantity-setter">
+              <input type="number" pattern="^-?[0-9]\d*\.?\d*$" />
+            </div>
+            <button onClick={()=> addBalance2()} style={{background:button2=='Redeemed'?"#32cd32":""}}>{button2}</button>
+          </div>
+        </section>
+        <section className="container">
+          <div>
+            <h1>Starbucks</h1>
+            <div className="pasha-image">
+              <img
+                alt="Random asset from Picsum"
+                src="https://logos-download.com/wp-content/uploads/2016/03/Starbucks_Logo_2011.png"
+                width="160"
+                height="160"
+              />
+            </div>
+          </div>
+          <div>
+            <p className="sr-legal-text">
+              Enter your Starbucks Gift Card Number
+            </p>
+            <div className="quantity-setter">
+              <input type="number" pattern="^-?[0-9]\d*\.?\d*$" />
+            </div>
+            <button onClick={()=> addBalance3()} style={{background:button3=='Redeemed'?"#32cd32":""}}>{button3}</button>
+          </div>
         </section>
       </div>
     </div>
